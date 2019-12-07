6abbe450d60095bdda1480704fc0d42855c356cc
Francis Chung <francis.c.chung@gmail.com>
2019-12-07 12:23:30 +1100

    Really hate when f**** examples dont work

--- protobuf-example-go/main.go
+++ protobuf-example-go/main.go
@@ -1,9 +1,22 @@
 package main
 
-import {
+import (
 	"fmt"
-}
+
+	"./src/simple"
+)
 
 func main() {
-	fmt.Println("hello world");
+	doSimple()
+}
+
+func doSimple() {
+	sm := example_simple.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My Simple Message",
+		SimpleList: []int32{1, 4, 7, 8},
+	}
+
+	fmt.Println(sm)
 }
